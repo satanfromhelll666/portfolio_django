9740f83f2f8fdee3804149e81a3146dc9e7e66c5
shakhawat hossain khan <mkhan172175@bscse.uiu.ac.bd>
2020-08-05 22:11:30 +0600

    Update settings.py

--- pw/settings.py
+++ pw/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'shakhawatkhan123@gmail.com'
-EMAIL_HOST_PASSWORD = 'onlyfortheweak'
+EMAIL_HOST_PASSWORD = '*********'
 EMAIL_PORT = 587
